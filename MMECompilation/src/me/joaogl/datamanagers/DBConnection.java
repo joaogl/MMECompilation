57b0eb564a1a01409b17a1c8d6e340dea08a995c
joaogl <joaoguerralourenco@hotmail.com>
2013-06-18 12:17:16 +0100

    Setting up the GNU General Public Licence.

--- MMECompilation/src/me/joaogl/datamanagers/DBConnection.java
+++ MMECompilation/src/me/joaogl/datamanagers/DBConnection.java
@@ -1,3 +1,20 @@
+/**
+ * This file is part of MMECompilation.
+
+    MMECompilation is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MMECompilation is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MMECompilation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.joaogl.datamanagers;
 
 /*import java.sql.DriverManager;*/
@@ -10,7 +27,7 @@ public class DBConnection {
 	public static Connection conn = null;
 	// public static String url =
 	// "jdbc:mysql://localhost:13306/MineApp?user=MasterMineApp&password=52jahH6asd88Aloq69z";
-	public static String url = "jdbc:mysql://176.28.53.39:13306/MineApp?user=MasterMineApp&password=52jahH6asd88Aloq69z";
+	public static String url = "jdbc:mysql://176.28.53.39:13306/MineApp?user=MasterMineApp&password=";
 	public static String xt;
 	public static Statement st;
 /*
